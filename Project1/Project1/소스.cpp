e0d5f56827c6eaf04e901b97c9e4d473005265a0
yeesonmin <yeesonmin@naver.com>
2020-02-25 09:05:40 +0900

    Update Study[카운팅 정렬] (백준 '수 정렬하기 3' 문제 풀기 : 메모리 초과, 동적메모리 해제 사용)

--- Project1/Project1/소스.cpp
+++ Project1/Project1/소스.cpp
@@ -6,7 +6,7 @@ using namespace std;
 //카운팅 정렬이용
 //참조 : https://bowbowbow.tistory.com/8
 
-void countingSort(int *count, int *arr, int n)
+void countingSort(int *count, int *arr, int n, int max)
 {
 	for (int i = 0; i < n; i++)
 	{
@@ -15,28 +15,36 @@ void countingSort(int *count, int *arr, int n)
 
 	}
 
-	//누적합
-	int countsum[10001] = { 0 };
-	countsum[0] = count[0];
-	for (int i = 1; i < 10001; i++)
+	for (int i = 0; i <= max; i++)
 	{
-		countsum[i] = countsum[i - 1] + count[i];
+		for (int j = 0; j < count[i]; j++)
+		{
+			cout << i << '\n';
+		}
 	}
 
-	//누적합에 따른 배열 만들기
-	int *sorted = new int[n];
-	for (int i = n-1; i >= 0; i--)
-	{
-		sorted[countsum[arr[i]]-1] = arr[i];
+	////누적합
+	//int *countsum = new int[max+1]{ 0 };
+	//countsum[0] = count[0];
+	//for (int i = 1; i < max+1; i++)
+	//{
+	//	countsum[i] = countsum[i - 1] + count[i];
+	//}
 
-		//해당 숫자 누적합 차감.
-		countsum[arr[i]]--;
-	}
+	////누적합에 따른 배열 만들기
+	//int *sorted = new int[n];
+	//for (int i = n-1; i >= 0; i--)
+	//{
+	//	sorted[countsum[arr[i]]-1] = arr[i];
 
-	for (int i = 0; i < n; i++)
+	//	//해당 숫자 누적합 차감.
+	//	countsum[arr[i]]--;
+	//}
+
+	/*for (int i = 0; i < n; i++)
 	{
 		cout << sorted[i] << '\n';
-	}
+	}*/
 
 }
 int main()
@@ -45,13 +53,19 @@ int main()
 	cin >> n;
 	
 	int *arr = new int[n];
-	int count[10001] = {0};
+	int max = 0;
+	
 
 	for (int i = 0; i < n; i++)
 	{
 		cin >> arr[i];
+		if (max < arr[i])
+		{
+			max = arr[i];
+		}
 	}
+	int *count = new int[max+1]{ 0 };
+	countingSort(count, arr, n, max);
 
-	countingSort(count, arr, n);
-
+	delete[] count,arr;
 }
