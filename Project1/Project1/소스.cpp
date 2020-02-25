0e9c6e5d812b2cd71b3e1c4ee89f1e4a67611bed
yeesonmin <yeesonmin@naver.com>
2020-02-26 08:41:25 +0900

    Update Study[카운팅 정렬] (백준 '수 정렬하기 3' 문제 풀기 : 성공, cin과 cout의 속도 해결)

--- Project1/Project1/소스.cpp
+++ Project1/Project1/소스.cpp
@@ -1,6 +1,4 @@
 ﻿#include <iostream>
-#include <algorithm>
-#include <vector>
 using namespace std;
 
 
@@ -8,22 +6,20 @@ int main()
 {
 	int n;
 	int count[10001] = { 0 };
-	int temp, max = 0;
+	int temp;
 
 	cin >> n;
 	for (int i = 0; i < n; i++)
 	{
-		cin >> temp;
+		scanf_s("%d", &temp);
 		count[temp]++;
-
-		max < temp ? max = temp : max;
 	}
 
-	for (int i = 1; i <= max; i++)
+	for (int i = 1; i <= 10000; i++)
 	{
 		for (int j = 0; j < count[i]; j++)
 		{
-			cout << i << '\n';
+			printf("%d\n", i);
 		}
 	}
 	
