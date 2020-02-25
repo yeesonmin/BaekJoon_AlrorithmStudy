e0637691d76b3450ff10ad6fe8fbd585da0ddf67
yeesonmin <yeesonmin@naver.com>
2020-02-26 08:21:29 +0900

    Update Study[카운팅 정렬] (백준 '수 정렬하기 3' 문제 풀기 : 시간 초과)

--- Project1/Project1/소스.cpp
+++ Project1/Project1/소스.cpp
@@ -4,15 +4,22 @@
 using namespace std;
 
 //카운팅 정렬이용
-//참조 : https://bowbowbow.tistory.com/8
 
-void countingSort(int *count, int *arr, int n, int max)
+int main()
 {
+	int n;
+	int count[10000] = { 0 };
+	int temp, max = 0;
+
+	cin >> n;
 	for (int i = 0; i < n; i++)
 	{
-		//정렬에 있는 숫자의 등장 횟수 세기
-		count[arr[i]]++;
-
+		cin >> temp;
+		count[temp]++;
+		if (max < temp)
+		{
+			max = temp;
+		}
 	}
 
 	for (int i = 0; i <= max; i++)
@@ -22,50 +29,5 @@ void countingSort(int *count, int *arr, int n, int max)
 			cout << i << '\n';
 		}
 	}
-
-	////누적합
-	//int *countsum = new int[max+1]{ 0 };
-	//countsum[0] = count[0];
-	//for (int i = 1; i < max+1; i++)
-	//{
-	//	countsum[i] = countsum[i - 1] + count[i];
-	//}
-
-	////누적합에 따른 배열 만들기
-	//int *sorted = new int[n];
-	//for (int i = n-1; i >= 0; i--)
-	//{
-	//	sorted[countsum[arr[i]]-1] = arr[i];
-
-	//	//해당 숫자 누적합 차감.
-	//	countsum[arr[i]]--;
-	//}
-
-	/*for (int i = 0; i < n; i++)
-	{
-		cout << sorted[i] << '\n';
-	}*/
-
-}
-int main()
-{
-	int n;
-	cin >> n;
 	
-	int *arr = new int[n];
-	int max = 0;
-	
-
-	for (int i = 0; i < n; i++)
-	{
-		cin >> arr[i];
-		if (max < arr[i])
-		{
-			max = arr[i];
-		}
-	}
-	int *count = new int[max+1]{ 0 };
-	countingSort(count, arr, n, max);
-
-	delete[] count,arr;
 }
