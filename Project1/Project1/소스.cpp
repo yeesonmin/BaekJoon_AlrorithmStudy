ab041d1fb5ac6f611f7658eb4ff45f7cb8cb9ba8
yeesonmin <yeesonmin@naver.com>
2020-02-26 09:05:57 +0900

    Add Study[백준] (2108 통계학 문제)

--- Project1/Project1/소스.cpp
+++ Project1/Project1/소스.cpp
@@ -7,25 +7,28 @@ int main()
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
+	/*
+	산술평균 : N개의 수들의 합을 N으로 나눈 값
+	중앙값 : N개의 수들을 증가하는 순서로 나열했을 경우 그 중앙에 위치하는 값
+	최빈값 : N개의 수들 중 가장 많이 나타나는 값
+	범위 : N개의 수들 중 최댓값과 최솟값의 차이
 
-	int n;
-	int count[10001] = { 0 };
+	N : 홀수
+
+	*/
+	int n, avg = 0, mid = 0, mcnt = 0, max = -4000, min = 4000;
+	int p[4001] = { 0 }, n[4001] = { 0 };
 	int temp;
-
+	
 	cin >> n;
+	int *arr = new int[n] { 0 };
+
 	for (int i = 0; i < n; i++)
 	{
-		
 		cin >> temp;
-		count[temp]++;
+
+
 	}
 
-	for (int i = 1; i <= 10000; i++)
-	{
-		for (int j = 0; j < count[i]; j++)
-		{
-			cout << i << '\n';
-		}
-	}
 	
 }
