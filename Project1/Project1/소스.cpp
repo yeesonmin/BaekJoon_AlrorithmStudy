66d6e6e8b1e2d143e8db0e59d085cf2c6d53f15b
yeesonmin <yeesonmin@naver.com>
2020-02-27 21:19:20 +0900

    Add Study[백준] (1427 소트인사이드 : 성공)

--- Project1/Project1/소스.cpp
+++ Project1/Project1/소스.cpp
@@ -1,5 +1,4 @@
 ﻿#include <iostream>
-#include <cmath>
 #include <algorithm>
 using namespace std;
 
@@ -9,53 +8,24 @@ int main()
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	
-	int n,  mode = 0, max = -4000, min = 4000;
-	double avg = 0;
-	int temp, modearr[8001] = {0}, mcnt = 0;
-	
+
+	int n;
+	int arr[11] = {0};
+
 	cin >> n;
-	int *arr = new int[n] { 0 };//정렬배열
 
-	for (int i = 0; i < n; i++)
+	int i = 0;
+	while (n>0)
 	{
-		cin >> temp;
-		avg += temp; //평균을 구하기 위해 더함.
-		max < temp ? max = temp : max;//최댓값
-		min > temp ? min = temp : min;//최솟값
-		arr[i] = temp;
-		
+		arr[i] = n % 10;
+		n = n / 10;
+		i++;
 	}
 
-	sort(arr, arr+n); //정렬
-	
-	for (int i = 0; i < n; i++)
-	{
-		modearr[arr[i]+4000]++; //등장수
-		mcnt < modearr[arr[i] + 4000] ? mcnt = modearr[arr[i] + 4000] : mcnt; //등장수 최댓값
-	}
+	sort(arr, arr + i);
 
-	temp = 0;
-	for (int i = 0; i < 8001; i++)
+	for (int j = i - 1; j >= 0; j--)
 	{
-		if (modearr[i] == mcnt)
-		{
-			if (temp > 1)
-			{
-				break;
-			}
-			else
-			{
-				mode = i - 4000; //최빈값
-				temp++;
-			}
-			
-		}
+		cout << arr[j];
 	}
-
-	//결과
-	cout << round(avg / n) << '\n';
-	cout << arr[n / 2] << '\n';
-	cout << mode << '\n';
-	cout << max - min << '\n';
 }
