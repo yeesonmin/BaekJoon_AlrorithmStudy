5a9e40286af60220839bac4bab028c9465e00875
yeesonmin <yeesonmin@gmail.com>
2020-03-02 11:20:01 +0900

    Review Study[힙 정렬]  (첫번째 복습)

--- Project1/Project1/소스.cpp
+++ Project1/Project1/소스.cpp
@@ -2,7 +2,7 @@
 #include <algorithm>
 using namespace std;
 
-//힙정렬
+//힙정렬 복습
 //동빈나 (https://blog.naver.com/ndb796/221228342808)
 
 
@@ -12,49 +12,50 @@ int main()
 	int heap[9] = { 7,6,5,8,3,5,9,1,6 };
 
 
-	//최대 힙 구조
+	//최대힙 구조 : 부모가 자식 보다 큰 구조
 	for (int i = 1; i < n; i++)
 	{
 		int c = i;
+
 		do {
-			int root = (c - 1) / 2; // 특정 원소의 부모
-			if (heap[root] < heap[c])
+			int root = (c - 1) / 2;
+			if (heap[c] > heap[root])
 			{
 				int temp = heap[root];
 				heap[root] = heap[c];
 				heap[c] = temp;
 			}
-			c = root; // 원소가 부모가 됨.
-
+			c = root;
 		} while (c != 0);
+		
+		
 	}
-	
-	//오른차순
+
+	//오름차순 힙 구조
 	for (int i = n - 1; i >= 0; i--)
 	{
 		int temp = heap[0];
 		heap[0] = heap[i];
 		heap[i] = temp;
+
 		int root = 0;
 		int c = 1;
 
-		//힙구조 만듦.
 		do {
 			c = 2 * root + 1;
-			// 자식 중 더 큰 값 찾기
-			if (heap[c] < heap[c + 1] && c < i - 1)
+			//자식 중 가장 큰 값 찾기, 마지막 자식은 제외(이미 가장 큰 수)
+			if (c < i-1 && heap[c] < heap[c + 1])
 			{
 				c++;
 			}
-			//루트보다 자식이 더 클 경우
-			if (heap[root] < heap[c]&& c<i)
+
+			if (heap[root] < heap[c] && c<i)
 			{
-				temp = heap[root];
-				heap[root] = heap[c];
-				heap[c] = temp;
+				temp = heap[c];
+				heap[c] = heap[root];
+				heap[root] = temp;
 			}
 			root = c;
-			
 		} while (c < i);
 		
 	}
