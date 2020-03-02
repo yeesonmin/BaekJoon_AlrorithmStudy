b76fd85b509b98b6a029be04e120f1572c01bb02
yeesonmin <yeesonmin@gmail.com>
2020-03-02 12:00:59 +0900

    Update Study[힙 정렬] (힙정렬 이해하기)

--- Project1/Project1/소스.cpp
+++ Project1/Project1/소스.cpp
@@ -3,8 +3,7 @@
 using namespace std;
 
 //힙정렬 복습
-//동빈나 (https://blog.naver.com/ndb796/221228342808)
-
+//나의 방식
 
 int main()
 {
@@ -13,52 +12,20 @@ int main()
 
 
 	//최대힙 구조 : 부모가 자식 보다 큰 구조
-	for (int i = 1; i < n; i++)
-	{
-		int c = i;
+	for (int i = 1; i < n; i++) {
 
-		do {
-			int root = (c - 1) / 2;
-			if (heap[c] > heap[root])
-			{
-				int temp = heap[root];
-				heap[root] = heap[c];
-				heap[c] = temp;
-			}
-			c = root;
-		} while (c != 0);
 		
+		if (heap[c] > heap[p])
+		{
+			int temp = heap[p];
+			heap[p] = heap[c];
+			heap[c] = temp;
+		}
 		
 	}
 
 	//오름차순 힙 구조
-	for (int i = n - 1; i >= 0; i--)
-	{
-		int temp = heap[0];
-		heap[0] = heap[i];
-		heap[i] = temp;
-
-		int root = 0;
-		int c = 1;
-
-		do {
-			c = 2 * root + 1;
-			//자식 중 가장 큰 값 찾기, 마지막 자식은 제외(이미 가장 큰 수)
-			if (c < i-1 && heap[c] < heap[c + 1])
-			{
-				c++;
-			}
-
-			if (heap[root] < heap[c] && c<i)
-			{
-				temp = heap[c];
-				heap[c] = heap[root];
-				heap[root] = temp;
-			}
-			root = c;
-		} while (c < i);
-		
-	}
+	
 
 	for (int i = 0; i < n; i++)
 	{
