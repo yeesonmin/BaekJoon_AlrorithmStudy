7446e9712af4ea3d12bdeb0ad2aecf10538e28f2
yeesonmin <yeesonmin@gmail.com>
2020-03-04 12:05:36 +0900

    Add Study[백준] (1181 단어 정렬 : 진행중)

--- Project1/Project1/소스.cpp
+++ Project1/Project1/소스.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <algorithm>
+#include <string>    //추가
 using namespace std;
 
 int main()
@@ -8,82 +9,91 @@ int main()
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int n;
-	int x[100000] = {0};
-	int y[100000] = { 0 };
+	int n, a = 0;
+	//문자열 클래스 사용
+	string st[20000];
+	string s;
+
 	cin >> n;
 	
-	//힙
 	for (int i = 0; i < n; i++)
 	{
-		cin  >> y[i] >> x[i];
+		cin >> s;
+		for (int j = 0; j <= i; j++)
+		{
+			if (st[j] == s)
+			{
+				a++;
+				break;
 
+			}
+			if(j==i)
+			{
+				st[i-a] = s;
+			}
+		}
+		
+
+		//힙정렬
 		if (i >= 1)
 		{
-			int c = i;
-
-			do {
+			int c = i - a;
+			do
+			{
 				int p = (c - 1) / 2;
-				if (x[c] > x[p])
+
+				if (st[c].length() > st[p].length())
 				{
-					int temp = x[c];
-					x[c] = x[p];
-					x[p] = temp;
-					int tempy = y[c];
-					y[c] = y[p];
-					y[p] = tempy;
+					string temp = st[c];
+					st[c] = st[p];
+					st[p] = temp;
 				}
 				c = p;
 			} while (c != 0);
 		}
 	}
-
 	
+	n = n -a;
 
-	//오름차순
+	//힙 오른차순 정렬
 	for (int i = n - 1; i >= 0; i--)
 	{
-		int temp = x[0];
-		x[0] = x[i];
-		x[i] = temp;
-		int tempy = y[0];
-		y[0] = y[i];
-		y[i] = tempy;
+		string temp = st[0];
+		st[0] = st[i];
+		st[i] = temp;
 
-		int c = 1;
-		int p = 0;
-
+		int c = 1, p = 0;
 		do {
 			c = 2 * p + 1;
-
-			if (x[c] < x[c+1] && c < i-1)
+			if (st[c].length() < st[c + 1].length() && c < i - 1)
 			{
 				c++;
 			}
 
-			if (x[c] > x[p]&& c<i)
+			if (st[c].length() > st[p].length() && c < i)
 			{
-				int temp = x[c];
-				x[c] = x[p];
-				x[p] = temp;
-				int tempy = y[c];
-				y[c] = y[p];
-				y[p] = tempy;
+				string temp = st[c];
+				st[c] = st[p];
+				st[p] = temp;
 			}
 			p = c;
-		} while (c < i);
 
+		} while (c < i);
 	}
 
-	//y정렬
+	//사전순으로 정렬
 	for (int i = 0; i < n; i++)
 	{
-		int xtemp = i;
-		
+		int cnt = 0, len = 0, tempi = i;
+
+		//문자열 길이가 같은 것 고르기.
 		while (true)
 		{
-			if (x[i] == x[i + 1] && i<n-1)
+
+			if (st[i].length() == st[i + 1].length() && i < n - 1)
 			{
+				len = st[i].length();
+				cnt++;
 				i++;
 			}
 			else
@@ -91,13 +101,32 @@ int main()
 				break;
 			}
 		}
-	
-		sort(y + xtemp, y + i+1);
-	
+		
+		//사전순
+		//왼쪽 글자 기준으로 정렬
+		if (len > 0)
+		{
+			for (int j = len - 1; j >= 0; j--)
+			{
+				for (int k = tempi; k < i - 1; k++)
+				{
+					for (int t = tempi; t < i - 1 - k; t++)
+					{
+						if ((int)st[k][j] > (int)st[k + 1][j])
+						{
+							string temp = st[k];
+							st[k] = st[k + 1];
+							st[k + 1] = temp;
+						}
+					}
+				}
+			}
+		}
+		
 	}
-	
+
 	for (int i = 0; i < n; i++)
-	{ 
-		cout << y[i] << " " << x[i]  << '\n';
+	{
+		cout<< st[i]<<'\n';
 	}
 }
