56d2d9eb3be05432c1e55725fb9c4a28f84c6b45
yeesonmin <yeesonmin@gmail.com>
2020-03-05 10:44:08 +0900

    Add Study[백준] (1181 단어 정렬 : 런타임 에러)

--- Project1/Project1/소스.cpp
+++ Project1/Project1/소스.cpp
@@ -11,13 +11,15 @@ int main()
 
 	int n, a = 0;
 	//문자열 클래스 사용
-	string st[20000];
 	string s;
+	string *st;
 
 	cin >> n;
-	
+	st = new string[n];
+
 	for (int i = 0; i < n; i++)
 	{
+		
 		cin >> s;
 		for (int j = 0; j <= i; j++)
 		{
@@ -53,77 +55,82 @@ int main()
 		}
 	}
 	
-	n = n -a;
-
-	//힙 오른차순 정렬
-	for (int i = n - 1; i >= 0; i--)
+	n = n - a;
+	if (n > 1)
 	{
-		string temp = st[0];
-		st[0] = st[i];
-		st[i] = temp;
-
-		int c = 1, p = 0;
-		do {
-			c = 2 * p + 1;
-			if (st[c].length() < st[c + 1].length() && c < i - 1)
-			{
-				c++;
-			}
-
-			if (st[c].length() > st[p].length() && c < i)
-			{
-				string temp = st[c];
-				st[c] = st[p];
-				st[p] = temp;
-			}
-			p = c;
-
-		} while (c < i);
-	}
-
-	//사전순으로 정렬
-	for (int i = 0; i < n; i++)
-	{
-		int cnt = 0, len = 0, tempi = i;
-
-		//문자열 길이가 같은 것 고르기.
-		while (true)
+		//힙 오른차순 정렬
+		for (int i = n - 1; i >= 0; i--)
 		{
+			string temp = st[0];
+			st[0] = st[i];
+			st[i] = temp;
 
-			if (st[i].length() == st[i + 1].length() && i < n - 1)
-			{
-				len = st[i].length();
-				cnt++;
-				i++;
-			}
-			else
-			{
-				break;
-			}
-		}
-		
-		//사전순
-		//왼쪽 글자 기준으로 정렬
-		if (len > 0)
-		{
-			for (int j = len - 1; j >= 0; j--)
-			{
-				for (int k = tempi; k < i - 1; k++)
+			int c = 1, p = 0;
+			do {
+				c = 2 * p + 1;
+				if (st[c].length() < st[c + 1].length() && c < i - 1)
 				{
-					for (int t = tempi; t < i - 1 - k; t++)
+					c++;
+				}
+
+				if (st[c].length() > st[p].length() && c < i)
+				{
+					string temp = st[c];
+					st[c] = st[p];
+					st[p] = temp;
+				}
+				p = c;
+
+			} while (c < i);
+
+
+		}
+
+		//사전순으로 정렬
+		for (int i = 0; i < n; i++)
+		{
+			int cnt = 0, len = 0, tempi = i;
+
+			//문자열 길이가 같은 것 고르기.
+			while (true)
+			{
+
+				if (st[i].length() == st[i + 1].length() && i < n - 1)
+				{
+					len = st[i].length();
+					cnt++;
+					i++;
+				}
+				else
+				{
+					break;
+				}
+			}
+
+			//사전순
+			//왼쪽 글자 기준으로 정렬
+			if (len > 0)
+			{
+				for (int j = len - 1; j >= 0; j--)
+				{
+					for (int k = tempi; k < i; k++)
 					{
-						if ((int)st[k][j] > (int)st[k + 1][j])
+						for (int t = tempi; t < i - k; t++)
 						{
-							string temp = st[k];
-							st[k] = st[k + 1];
-							st[k + 1] = temp;
+							if ((int)st[t][j] > (int)st[t + 1][j])
+							{
+								string temp = st[t];
+								st[t] = st[t + 1];
+								st[t + 1] = temp;
+							}
 						}
 					}
 				}
 			}
+
 		}
-		
 	}
+	
 
 	for (int i = 0; i < n; i++)
 	{
