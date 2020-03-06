4f5bb943f7af63cb7ad869d2b209f8a979c1defb
yeesonmin <yeesonmin@gmail.com>
2020-03-06 09:21:52 +0900

    Update Study[백준] (10814 나이순 정렬 : 성공)

--- Project1/Project1/소스.cpp
+++ Project1/Project1/소스.cpp
@@ -4,14 +4,25 @@
 #include <string>
 using namespace std;
 
+bool compare(pair<int, pair<int, string>> a, pair<int, pair<int, string>> b)
+{
+	if (a.first == b.first)
+	{
+		return a.second.first < b.second.first;
+	}
+	else
+	{
+		return a.first < b.first;
+	}
+}
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int n;
-	vector<pair<int, string>>user;
+	int n, cnt = 0;
+	vector<pair<int,pair<int, string>>>user;
 
 	//회원수
 	cin >> n;
@@ -22,14 +33,15 @@ int main()
 		int a;
 		string b;
 		cin >> a >> b;
-		user.push_back(pair<int, string>(a, b));
+		cnt++;
+		user.push_back(pair<int, pair<int, string>>(a,make_pair(cnt,b)));
 	}
 	
 	//정렬
-	sort(user.begin(), user.end());
+	sort(user.begin(), user.end(), compare);
 
 	for (int i = 0; i < n; i++)
 	{
-		cout << user[i].first << " " << user[i].second << '\n';
+		cout << user[i].first << " " << user[i].second.second << '\n';
 	}
 }
