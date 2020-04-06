86422195c30ec2d36b667eb2708b11ddb80be8b5
yeesonmin <yeesonmin@gmail.com>
2020-04-06 17:00:44 +0900

    Update Study[백트래킹] (박킹 블로그 참조, 코드 분석)

--- Project1/Project1/소스.cpp
+++ Project1/Project1/소스.cpp
@@ -1,27 +1,30 @@
 ﻿#include <bits/stdc++.h>
-
 using namespace std;
 
-//백트래킹 예제 연습
-//참조 : https://blog.encrypted.gg/732
-
 int n, m;
-void Backtracking(int k, int *arr, bool *isused)
-{
+void func(int* arr, int k) { // 현재 k개까지 수를 택했음.
 	
-	
+	if (k == m) { // m개를 모두 택했으면
+		for (int i = 0; i < m; i++) 
+			cout << arr[i] + 1 << ' '; // arr에 기록해둔 수를 출력.(0부터 m-1까지가 아니라 1부터 m까지이므로 +1을 해줌)
+		cout << '\n';
+		return;
+	}
+
+	int st = 0;
+	if (k != 0) st = arr[k - 1];
+
+	for (int i = st; i < n; i++) { // 1부터 n까지의 수에 대해
+		arr[k] = i; // k번째 수를 i로 정함
+		func(arr, k + 1); // 다음 수를 정하러 한 단계 더 들어감      
+	}
 }
-int main(void)
-{
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
 	cin >> n >> m;
-	bool isused[n] = { false }; //각 수 사용 여부
-	int k = 0, arr[m] = { 0 };//K: 리스트에 있는 수의 갯수, arr : 리스트
-
-	
-
-
+	int *arr = new int[m] {};
+	func(arr, 0);
 }
