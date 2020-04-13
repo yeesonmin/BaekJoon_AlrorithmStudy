9eb26b482f302828d2a674cb5e12c4b55c85ea88
yeesonmin <yeesonmin@gmail.com>
2020-04-13 15:47:46 +0900

    Update Study[백트래킹] (박킹 블로그 참조, 코드 분석, 해결)

--- Project1/Project1/소스.cpp
+++ Project1/Project1/소스.cpp
@@ -2,29 +2,41 @@
 using namespace std;
 
 int n, m;
-void func(int* arr, int k) { // 현재 k개까지 수를 택했음.
-	
-	if (k == m) { // m개를 모두 택했으면
-		for (int i = 0; i < m; i++) 
-			cout << arr[i] + 1 << ' '; // arr에 기록해둔 수를 출력.(0부터 m-1까지가 아니라 1부터 m까지이므로 +1을 해줌)
+void backtraking(int *arr, bool *cNumber, int k)
+{
+	if (k == m)
+	{
+		for (int i = 0; i < m; i++)
+		{
+			cout << arr[i] << ' ';
+		}
 		cout << '\n';
 		return;
 	}
 
-	int st = 0;
-	if (k != 0) st = arr[k - 1];
-
-	for (int i = st; i < n; i++) { // 1부터 n까지의 수에 대해
-		arr[k] = i; // k번째 수를 i로 정함
-		func(arr, k + 1); // 다음 수를 정하러 한 단계 더 들어감      
+	for (int i = 0; i < n; i++)
+	{
+		if (!cNumber[i])
+		{
+			arr[k] = i + 1;
+			cNumber[i] = 1;
+			backtraking(arr, cNumber, k + 1);
+			cNumber[i] = 0;
+		}
 	}
+
+
 }
-int main(void) {
+
+int main()
+{
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
-
 	cin >> n >> m;
-	int *arr = new int[m] {};
-	func(arr, 0);
+
+	int *arr = new int[m]{ 0 };
+	bool *cNumber = new bool[n]{ 0 };
+	backtraking(arr, cNumber, 0);
+
 }
