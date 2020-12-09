059b09795aa35383d8c867de494c2194ad40d289
DESKTOP-318JVPJ\yeeso <yeesonmin@naver.com>
2020-12-09 14:36:29 +0900

    Add Code[이것이 코딩 테스트다 문제해결 코드] (심화문제 11.6(ing))

--- 이것이 코딩 테스트다/Project1/Project1/소스.cpp
+++ 이것이 코딩 테스트다/Project1/Project1/소스.cpp
@@ -5,37 +5,68 @@ using namespace std;
 
 int solution(vector<int> food_times, long long k) {
     int answer = 0;
+    int remain = k % food_times.size();
+    int q = k / food_times.size();
 
-    answer = k % food_times.size();//이상적인 위치
+    answer = remain-1; //예상위치
 
-    //무지가 음식을 먹음
-    for (int i = 0; i < food_times.size(); i++)
-    {
-        food_times[i] -= k / food_times.size();
-    }
-    for (int i = 0; i < food_times.size() % k; i++)
-    {
-        food_times[i]--;
+    long long sum = 0;
+    for (int i = 0; i < food_times.size(); i++) {
+        sum += food_times[i];
     }
 
-    for (int i = 0; i < food_times.size(); i++)
-    {
-        if (food_times[answer] > 0)
-        {
-            return answer + 1;
+    if (sum <= k){
+        return -1;
+    }
+    
+    int remainch = remain;
+    for (int i = 0; i < food_times.size(); i++) {
+        if (remainch > 0) {
+            if (food_times[i] - (q + 1) >= 0) {
+                food_times[i] -= (q + 1);
+                remainch--;
+            }
+            else
+            {
+                food_times[i] = 0;
+
+            }
+            
+        }
+
+    }
+
+
+    for (int i = 0; i < food_times.size(); i++){
+        if (answer <= remain-1){
+            if (food_times[answer] - (q + 1) < 0) {
+                answer++;
+                if (answer == food_times.size())
+                {
+                    answer = 0;
+                }
+            }
+            else
+            {
+                return answer+1;
+            }
         }
         else
         {
-            answer++;
-            if (answer == food_times.size())
+            if (food_times[answer] - q < 0) {
+                answer++;
+                if (answer == food_times.size())
+                {
+                    answer = 0;
+                }
+            }
+            else
             {
-                answer = 0;
+                return answer+1;
             }
         }
+        
     }
-
-    return -1;
-
 }
 
 void main()
