7c6fea6e3bae2f27ed5fbf9d28028dcbf2475c65
DESKTOP-318JVPJ\yeeso <yeesonmin@naver.com>
2020-12-21 14:00:51 +0900

    Add Code[이것이 코딩 테스트다 문제해결 코드] (5.1 ~ 5.9)

--- 이것이 코딩 테스트다/Project1/Project1/소스.cpp
+++ 이것이 코딩 테스트다/Project1/Project1/소스.cpp
@@ -1,74 +1,57 @@
 #include <iostream>
+#include <vector>
+#include <queue>
 
 using namespace std;
 
-void main()
-{
-	int sizex = 0, sizey = 0;
-	int cx = 0, cy = 0, cf = 0;
-	int arr[50][50];
-	int c[50][50] = { 0 };
-	int cnt = 1;
+bool visited[9];
+vector<int> graph[9];
 
+void dfs(int x) {
+    // 현재 노드를 방문 처리
+    visited[x] = true;
+    cout << x << ' ';
+    // 현재 노드와 연결된 다른 노드를 재귀적으로 방문
+    for (int i = 0; i < graph[x].size(); i++) {
+        int y = graph[x][i];
+        if (!visited[y]) dfs(y);
+    }
+}
 
-	cin >> sizey >> sizex;
-	cin >> cy >> cx >> cf;
+int main(void) {
+    // 노드 1에 연결된 노드 정보 저장 
+    graph[1].push_back(2);
+    graph[1].push_back(3);
+    graph[1].push_back(8);
 
-	for (int i = 0; i < sizey; i++) {
-		for (int j = 0; j < sizex; j++) {
-			int x;
-			cin >> x;
-			arr[i][j] = x;
-		}
-	}
+    // 노드 2에 연결된 노드 정보 저장 
+    graph[2].push_back(1);
+    graph[2].push_back(7);
 
+    // 노드 3에 연결된 노드 정보 저장 
+    graph[3].push_back(1);
+    graph[3].push_back(4);
+    graph[3].push_back(5);
 
-	int dx[4] = { 0, 1, 0, -1 };
-	int dy[4] = { -1, 0, 1, 0 };
+    // 노드 4에 연결된 노드 정보 저장 
+    graph[4].push_back(3);
+    graph[4].push_back(5);
 
-	c[cy][cx] = 1;
+    // 노드 5에 연결된 노드 정보 저장 
+    graph[5].push_back(3);
+    graph[5].push_back(4);
 
-	int check = 0;
-	while (true)
-	{
-		if (cf - 1 == -1)
-		{
-			cf = 3;
-		}
-		else
-		{
-			cf--;
-		}
+    // 노드 6에 연결된 노드 정보 저장 
+    graph[6].push_back(7);
 
-		if (arr[cy + dy[cf]][cx + dx[cf]] == 0 && c[cy + dy[cf]][cx + dx[cf]] == 0)
-		{
-			check = 0;
-			cnt++;
-			cy += dy[cf];
-			cx += dx[cf];
-			c[cy][cx] = 1;
-		}
-		else
-		{
-			check++;
-		}
+    // 노드 7에 연결된 노드 정보 저장 
+    graph[7].push_back(2);
+    graph[7].push_back(6);
+    graph[7].push_back(8);
 
-		if (check == 4)
-		{
-			if (arr[cy - dy[cf]][cx - dx[cf]] == 1)
-			{
-				break;
-			}
-			else
-			{
-				cy -= dy[cf];
-				cx -= dx[cf];
-				check = 0;
-			}
-			
-		}
-	}
+    // 노드 8에 연결된 노드 정보 저장 
+    graph[8].push_back(1);
+    graph[8].push_back(7);
 
-	cout << cnt;
-
+    dfs(1);
 }
