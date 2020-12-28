ded202cb2336f213850d084524d34d583d49376c
DESKTOP-318JVPJ\yeeso <yeesonmin@naver.com>
2020-12-28 15:23:18 +0900

    Add Code[이것이 코딩 테스트다 문제해결 코드] (5.10 ~5.11)

--- 이것이 코딩 테스트다/Project1/Project1/소스.cpp
+++ 이것이 코딩 테스트다/Project1/Project1/소스.cpp
@@ -1,64 +1,53 @@
 #include <iostream>
-#include <vector>
 #include <queue>
 using namespace std;
 
-int dx[4] = { 0, 0, -1, 1 }, dy[4] = {-1,1,0,0};
-vector<pair <int, int>> node[1000000];
-bool visited[1000000] = { false };
 
+int dx[4] = {0, 0, -1, 1};
+int dy[4] = {-1, 1, 0, 0};
 
 void main()
 {
-	bool map[1000][1000];
-	int n, m;
-	int cnt = 0, num = 0;
+	int map[200][200];
+	int n = 0, m = 0;
 
 	cin >> n >> m;
-	for (int i = 0; i < n; i++)
-	{
+	
+	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			cin >> map[i][j];
-			if (map[i][j] == 0) {
-				node[num].push_back({ i,j });
-				num++;
-			}
+			scanf_s("%1d", &map[i][j]);
 		}
 	}
 
-	for (int i = 0; i < num; i++) {
-		for (int j = 0; j < num; j++) {
-			if (i == j) {
-				break;
-			}
+	queue<pair <int, int>> q;
+	q.push({ 0, 0 });
+	int cnt = 1;
+	int x, y;
 
-			for (int k = 0; k < 4; k++) {
-				if (node[i][0].first + dx[k] == node[j][0].first && node[i][0].second + dx[k] == node[j][0].second) {
-					node[i].push_back({ j , 0});
-				}
-			}
-		}
-	}
-
-	queue<int> q;
-	q.push(0);
-	visited[0] = true;
-
-	for (int i = 0; i < num; i++)
-	{
-
-	}
+	bool sizeChange = true;
 	while (!q.empty())
 	{
-		int x = q.front();
-		q.pop();
-		for (int i = 1; i < node[x].size(); i++) {
-			int y = node[x][i].first;
-			if (!visited[y]) {
-				q.push(y);
-				visited[y] = true;
-			}
+		if (sizeChange) {
+			cnt++;
+			sizeChange = false;
 		}
+		
+		for (int i = 0; i < 4; i++) {
+			x = q.front().second + dx[i];
+			y = q.front().first + dy[i];
+			if ((x >= 0 && x < m) && (y >= 0 && y < n))
+			{
+				if (map[y][x] == 1) {
+					q.push({ y , x });
+					sizeChange = true;
+					map[y][x] = cnt;
+				}
+			}
+			
+		}
+		q.pop();
 	}
-	cnt++;
+
+	cout << map[n - 1][m - 1];
+
 }
