607edd72bda1f7eb4800be32bee040559b85dba1
DESKTOP-318JVPJ\yeeso <yeesonmin@naver.com>
2021-01-25 14:44:55 +0900

    Add Code[이것이 코딩 테스트다 문제해결 코드] (9.02 ~ 9.03 복습)

--- 이것이 코딩 테스트다/Project1/Project1/소스.cpp
+++ 이것이 코딩 테스트다/Project1/Project1/소스.cpp
@@ -1,89 +1,49 @@
 #include <iostream>
-#include<vector>
-
 #define INF 1e9
 using namespace std;
 
-//간단한 다익스트라 복습
-
-//노드수, 간선수, 시작
-int n, m, start;
-//노드
-vector<pair<int, int>>graph[100001];
-//테이블
-int d[100001];
-//방문기록
-bool visited[100001];
-
-
-int getSmallestNode()
-{
-	//연결된 노드 중 거리가 가장 가깝고 방문 안한 노드 찾기
-	int minValue = INF;
-	int index = 0;
-
-	for (int i = 1; i <= n; i++) {
-		if (d[i] < minValue && !visited[i]) {
-			minValue = d[i];
-			index = i;
-		}
-	}
-
-	return index;
-}
-
-void dijkstra(int start) {
-	//시작값 초기화
-	d[start] = 0;
-	visited[start] = true;
-
-	//테이블 작성
-	for (int i = 0; i < graph[start].size(); i++) {
-		d[graph[start][i].first] = graph[start][i].second;
-	}
-
-	//나머지 테이블 작성
-	for (int i = 0; i < n - 1; i++) {
-		int now = getSmallestNode();
-		visited[now] = true;
-
-		for (int i = 0; i < graph[now].size(); i++) {
-			int cost = d[now] + graph[now][i].second;
-			if (cost < d[graph[now][i].first]) {
-				d[graph[now][i].first] = cost;
-			}
-			
-		}
-	}
-
-}
 
 void main() {
+	int n, m;
+	cin >> n >> m;
 
+	int graph[501][501];
 
-	cin >> n >> m >> start;
-
-	//노드 연결
-	for (int i = 0; i < m; i++)
-	{
-		//a : 시작노드, b: 도착노드, c : 거리
-		int a, b, c;
-		cin >> a >> b >> c;
-		graph[a].push_back({ b,c });
+	for (int i = 1; i < 501; i++) {
+		fill(graph[i], graph[i] + 501, INF);
 	}
 
-	//테이블 무한으로 초기화
-	fill_n(d, 100001, INF);
+	for (int i = 1; i < 501; i++) {
+		for (int j = 1; j < 501; j++) {
+			if (i == j) graph[i][j] = 0;
+		}
+	}
 
-	dijkstra(start);
+	for (int i = 0; i < m; i++) {
+		int a, b, c;
+		cin >> a >> b >> c;
+		graph[a][b] = c;
+	}
 
 	for (int i = 1; i <= n; i++) {
-		if (d[i] == INF) {
-			cout << "INF" << ' ';
+		for (int a = 1; a <= n; a++) {
+			for (int b = 1; b <= n; b++) {
+				graph[a][b] = min(graph[a][b], graph[a][i] + graph[i][b]);
+			}
 		}
-		else
+	}
+	
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++)
 		{
-			cout << d[i];
+			if (graph[i][j] == INF) {
+				cout << INF << ' ';
+			}
+			else
+			{
+				cout << graph[i][j] << ' ';
+			}
 		}
+		cout << '\n';
 	}
 }
