0aa2572b991c3c1b339ce98e994ad8f26ae71744
DESKTOP-318JVPJ\yeeso <yeesonmin@naver.com>
2021-01-26 15:04:56 +0900

    Add Code[이것이 코딩 테스트다 문제해결 코드] (9.04 ing)

--- 이것이 코딩 테스트다/Project1/Project1/소스.cpp
+++ 이것이 코딩 테스트다/Project1/Project1/소스.cpp
@@ -1,49 +1,78 @@
 #include <iostream>
+#include <queue>
+#include <vector>
 #define INF 1e9
 using namespace std;
 
+int d[101];
+priority_queue<pair<int, int>> pq;
+vector<int> node[101];
+int result = 0;
 
+void first() {
+	fill(d, d + 101, 0);
+	d[1] = 0;
+	pq.push({ 0, 1 });
+
+	while (!pq.empty())
+	{
+		int dist = pq.top().first;
+		int now = pq.top().second;
+		pq.pop();
+		if (d[now] != 0 && d[now] < dist) continue;
+		for (int i = 0; i < node[now].size(); i++) {
+			int cost = dist + 1;
+			if (cost < d[node[now][i]]) {
+				d[node[now][i]] = cost;
+				pq.push({ cost , node[now][i] });
+			}
+		}
+	}
+
+}
+
+void second(int start) {
+
+
+	fill(d, d + 101, 0);
+	d[start] = 0;
+	pq.push({ 0, start });
+
+	while (!pq.empty())
+	{
+		int dist = pq.top().first;
+		int now = pq.top().second;
+		pq.pop();
+		if (d[now] != 0 && d[now] < dist) continue;
+		for (int i = 0; i < node[now].size(); i++) {
+			int cost = dist + 1;
+			if (cost < d[node[now][i]]) {
+				d[node[now][i]] = cost;
+				pq.push({ cost , node[now][i] });
+			}
+		}
+	}
+
+}
 void main() {
 	int n, m;
+	int x, k;
+
 	cin >> n >> m;
 
-	int graph[501][501];
-
-	for (int i = 1; i < 501; i++) {
-		fill(graph[i], graph[i] + 501, INF);
-	}
-
-	for (int i = 1; i < 501; i++) {
-		for (int j = 1; j < 501; j++) {
-			if (i == j) graph[i][j] = 0;
-		}
-	}
-
 	for (int i = 0; i < m; i++) {
-		int a, b, c;
-		cin >> a >> b >> c;
-		graph[a][b] = c;
+		int a, b;
+		cin >> a >> b;
+		node[a].push_back(b);
 	}
 
-	for (int i = 1; i <= n; i++) {
-		for (int a = 1; a <= n; a++) {
-			for (int b = 1; b <= n; b++) {
-				graph[a][b] = min(graph[a][b], graph[a][i] + graph[i][b]);
-			}
-		}
-	}
-	
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++)
-		{
-			if (graph[i][j] == INF) {
-				cout << INF << ' ';
-			}
-			else
-			{
-				cout << graph[i][j] << ' ';
-			}
-		}
-		cout << '\n';
-	}
+	cin >> x >> k;
+
+	first();
+	result = d[k];
+
+	second(k);
+	result += d[x];
+
+	cout << result;
 }
